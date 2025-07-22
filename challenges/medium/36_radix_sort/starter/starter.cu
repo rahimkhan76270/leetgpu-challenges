6c67cdf7f55b79da0f1945f0eb2754285f16f9ab
Ishaan Arya <iarya@umich.edu>
2025-07-23 00:49:25 +0530

    fixed radix sort starter code

--- challenges/medium/36_radix_sort/starter/starter.cu
+++ challenges/medium/36_radix_sort/starter/starter.cu
@@ -1,7 +1,6 @@
-#include "solve.h"
 #include <cuda_runtime.h>
 
 // input, output are device pointers
-void solve(const unsigned int* input, unsigned int* output, int N) {
+extern "C" void solve(const unsigned int* input, unsigned int* output, int N) {
             
 } 
