1d73ec8781c41a24075fce5155a3476f0560ceb0
Taoyu Zhu <z609495@gmail.com>
2025-08-04 17:38:18 +0800

    fix 37 matrix power cuda start template (#22)
    * sparse matrix vector multi atol and rtol from 1e-5 -> 1e-3
    
    * fix 37_matrix_power cuda template

--- challenges/medium/37_matrix_power/starter/starter.cu
+++ challenges/medium/37_matrix_power/starter/starter.cu
@@ -1,7 +1,6 @@
-#include "solve.h"
 #include <cuda_runtime.h>
 
 // input, output are device pointers
-void solve(const float* input, float* output, int N, int P) {
+extern "C" void solve(const float* input, float* output, int N, int P) {
 
 } 
