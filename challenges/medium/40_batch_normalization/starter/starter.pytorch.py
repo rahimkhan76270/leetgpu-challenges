f3c4eae4ab547ae9b0f64c9e3d03134b5b56d730
Kunal Mansukhani <kmmansukhani@gmail.com>
2025-08-13 00:51:49 -0400

    added batch norm challenge (#29)

---
+++ challenges/medium/40_batch_normalization/starter/starter.pytorch.py
@@ -0,0 +1,6 @@
+import torch
+
+# input, gamma, beta, output are tensors on the GPU
+def solve(input: torch.Tensor, gamma: torch.Tensor, beta: torch.Tensor, 
+          output: torch.Tensor, N: int, C: int, eps: float):
+    pass
