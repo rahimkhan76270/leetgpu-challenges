e3dde50a19d0843bf3a57d0b7b14475a4d7903ea
Kunal Mansukhani <kmmansukhani@gmail.com>
2025-08-15 04:11:00 -0400

    [Count Array Elements] Fix comments (#40)

--- challenges/easy/43_count_array_element/starter/starter.triton.py
+++ challenges/easy/43_count_array_element/starter/starter.triton.py
@@ -6,7 +6,7 @@ import triton.language as tl
 def count_equal_kernel(input_ptr, output_ptr, N, K, BLOCK_SIZE: tl.constexpr):
     pass
 
-# input, output are tensors on the GPU (i.e. tensors on the GPU)
+# input, output are tensors on the GPU
 def solve(input: torch.Tensor, output: torch.Tensor, N: int, K: int):
     BLOCK_SIZE = 256
     grid = lambda meta: (triton.cdiv(N, meta['BLOCK_SIZE']),)
