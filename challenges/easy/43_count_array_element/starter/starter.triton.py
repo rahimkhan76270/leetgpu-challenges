d25bbc77425f58a35a59b4124d3610df7a08d5a9
SungHwanYun <36258098+SungHwanYun@users.noreply.github.com>
2025-08-15 17:06:08 +0900

    add count array element challenge problem (#37)

---
+++ challenges/easy/43_count_array_element/starter/starter.triton.py
@@ -0,0 +1,13 @@
+import torch
+import triton
+import triton.language as tl
+
+@triton.jit
+def count_equal_kernel(input_ptr, output_ptr, N, K, BLOCK_SIZE: tl.constexpr):
+    pass
+
+# input, output are tensors on the GPU (i.e. tensors on the GPU)
+def solve(input: torch.Tensor, output: torch.Tensor, N: int, K: int):
+    BLOCK_SIZE = 256
+    grid = lambda meta: (triton.cdiv(N, meta['BLOCK_SIZE']),)
+    count_equal_kernel[grid](input, output, N, K, BLOCK_SIZE=BLOCK_SIZE)
